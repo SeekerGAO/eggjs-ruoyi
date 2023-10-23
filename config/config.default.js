aecfa9bfdde8f24cff2f79c839693c427e0a847e
SeekerGAO <1532584997@qq.com>
2023-10-23 16:38:19 +0800

    feat: mysql config

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,25 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: 'root',
+      // 数据库名
+      database: 'ry-vue',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  });
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1698049631342_192';
